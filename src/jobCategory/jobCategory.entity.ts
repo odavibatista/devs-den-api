2f075c50759679d7385d638fbd6b63e181b39d8c
Davi Batista <davideosmar11@gmail.com>
2024-04-11 16:39:07 -0300

    chore: added pull request template

--- src/jobCategory/jobCategory.entity.ts
+++ src/jobCategory/jobCategory.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class Categories  {
+export class JobCategories  {
     @PrimaryGeneratedColumn()
     id_category: number
 
