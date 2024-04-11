97ee3e44f62454a2b607a7f7132ccc22f2c91448
Davi Batista <davideosmar11@gmail.com>
2024-04-10 21:48:01 -0300

    feat: created first credentials DTOs, created skill entity

---
+++ src/loginCredentials/loginCredentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsEnum, IsString, Length } from 'class-validator'
+import { Unique } from 'typeorm'
+
+export class CreateCredentialsDTO   {
+    @IsString()
+    @Length(10, 50)
+    @Unique(["email"])
+    readonly email: string
+
+    @IsString()
+    @Length(10, 250)
+    readonly password: string
+
+    @IsEnum(["company", "candidate"])
+    readonly role: string
+}
