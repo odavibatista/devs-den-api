f6cedf511262fb0d03b77ce2190f5b4be88451b7
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-04-29 21:21:20 -0300

    feat: created companies controller

--- src/modules/company/controller/company.controller.ts
+++ src/modules/company/controller/company.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CompanyService } from '../service/company.service';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+    constructor(
+        private readonly companyService: CompanyService
+    )   {}
+
+    @Get()
+    async findAll(): Promise<any[]> {
+        return this.companyService.findAll()
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<any>    {
+        return this.companyService.findOne(id)
+    }
+}
