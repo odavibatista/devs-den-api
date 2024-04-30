f32e766d9c2ea1b7aacc90610368c3a680fa7088
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-04-30 20:06:36 -0300

    feat: created job cateogry service

--- src/modules/job-category/job-category.module.ts
+++ src/modules/job-category/job-category.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JobCategoryController } from './controller/job-category.controller';
 import { JobCategoryService } from './service/job-category.service';
+import { CategoryNotFoundException } from './domain/errors/CategoryNotFound.exception';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JobCategory } from './entity/job-category';
+import { Job } from '../job/entity/job.entity';
 
 @Module({
+  imports: [
+    CategoryNotFoundException,
+    JobCategoryModule,
+    TypeOrmModule.forFeature([JobCategory]),
+  ],
   controllers: [JobCategoryController],
   providers: [JobCategoryService]
 })
--- src/modules/job-category/service/job-category.service.ts
+++ src/modules/job-category/service/job-category.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JobCategory } from '../entity/job-category';
+import { Repository } from 'typeorm';
+import { CategoryNotFoundException } from '../domain/errors/CategoryNotFound.exception';
 
 @Injectable()
-export class JobCategoryService {}
+export class JobCategoryService {
+    constructor(
+        @InjectRepository(JobCategory)
+        private jobCategoryRepository: Repository<JobCategory>,
+    )   {}
+
+    async findAll (): Promise<JobCategory[] | CategoryNotFoundException>  {
+        const jobCategories =  await this.jobCategoryRepository.find()
+
+        if(jobCategories.length === 0) throw new CategoryNotFoundException()
+
+        else return jobCategories
+    }
+
+    async findOne (id: number): Promise<JobCategory | CategoryNotFoundException>  {
+        const jobCategory = await this.jobCategoryRepository.findOne({
+            where: { id_category: id}
+        })
+
+        if (!jobCategory) throw new CategoryNotFoundException()
+
+        else return jobCategory
+    }
+}
