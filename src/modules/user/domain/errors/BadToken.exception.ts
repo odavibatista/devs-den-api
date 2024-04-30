4bde95db1c44446a9efbb374d828c1d94a18fd9f
Davi Batista <davideosmar11@gmail.com>
2024-04-30 18:03:17 -0300

    feat: created exceptions for user module

---
+++ src/modules/user/domain/errors/BadToken.exception.ts
@@ -0,0 +1,7 @@
+import { HttpException, HttpStatus } from '@nestjs/common'
+
+export class BadTokenException extends HttpException {
+  constructor() {
+    super('Token inválido.', HttpStatus.UNAUTHORIZED)
+  }
+}
---
+++ src/modules/user/domain/errors/UnformattedPassword.exception.ts
@@ -0,0 +1,7 @@
+import { HttpException, HttpStatus } from '@nestjs/common'
+
+export class UnformattedPasswordException extends HttpException {
+    constructor() {
+        super('Sua senha é muito fraca. Insira outra.', HttpStatus.NOT_ACCEPTABLE)
+    }
+}
