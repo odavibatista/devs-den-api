b8752bb94241e10750bdfc7c11a8e76c8216b7df
Davi Batista <davideosmar11@gmail.com>
2024-04-30 22:49:22 -0300

    docs: added swagger docs to user endpoints

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -3,6 +3,7 @@ import {
     Controller,
     Delete,
     Get,
+    HttpStatus,
     Param,
     Post,
     Put,
@@ -12,30 +13,75 @@ import {
 import { JWTProvider } from '../providers/JWT.provider';
 import { UserNotFoundException } from '../domain/errors/UserNotFound.exception';
 import { User } from '../entity/user.entity';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AllExceptionsFilterDTO } from 'src/shared/domain/dtos/errors/AllException.filter.dto';
+import { UnformattedEmailException } from '../domain/errors/UnformattedEmail.exception';
+import { UnformattedPasswordException } from '../domain/errors/UnformattedPassword.exception';
+import { EmailAlreadyRegisteredException } from '../domain/errors/EmailAlreadyRegistered.exception';
+import { WrongPasswordException } from '../domain/errors/WrongPassword.exception';
   
   @Controller('user')
+  @ApiTags('Usuário')
   export class UserController {
     constructor(
       private readonly userService: UserService,
       private JwtProvider: JWTProvider
     ) {}
   
-    @Get()
-    async findAll(): Promise<any[] | UserNotFoundException> {
-      return this.userService.findAll();
-    }
-  
     @Get(':id')
+    @ApiResponse({
+      status: new UserNotFoundException().getStatus(),
+      description: new UserNotFoundException().message,
+      type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+      status: HttpStatus.OK,
+      description: 'Usuário encontrado com sucesso',
+    })
     async findOne(@Param('id') id: number): Promise<User | UserNotFoundException> {
       return this.userService.findOne(id);
     }
   
     @Post('/register')
+    @ApiResponse({
+      status: new UnformattedEmailException().getStatus(),
+      description: new UnformattedEmailException().message,
+      type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+      status: new UnformattedPasswordException().getStatus(),
+      description: new UnformattedPasswordException().message,
+      type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+      status: new EmailAlreadyRegisteredException().getStatus(),
+      description: new EmailAlreadyRegisteredException().message,
+      type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+      status: HttpStatus.CREATED,
+      description: 'Usuário criado com sucesso',
+    })
     async create(@Body() createUserDto: CreateUserDTO): Promise<any> {
       return this.userService.create(createUserDto);
     }
 
     @Post('login')
+    @ApiResponse({
+      status: new UserNotFoundException().getStatus(),
+      description: new UserNotFoundException().message,
+      type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+      status: new UnformattedEmailException().getStatus(),
+      description: new UnformattedEmailException().message,
+      type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+      status: new WrongPasswordException().getStatus(),
+      description: new WrongPasswordException().message,
+      type: AllExceptionsFilterDTO
+    })
     async login(@Body() loginDto: LoginDTO): Promise<any> {
       return this.userService.login(loginDto);
     }
