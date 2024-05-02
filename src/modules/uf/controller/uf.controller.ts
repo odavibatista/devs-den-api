315231aabec5ee7cff466e114fedd03a2cc2eece
Davi Batista <davideosmar11@gmail.com>
2024-05-02 20:51:01 -0300

    feat: added swagger docs for UFs' GET endpoint

--- src/modules/uf/controller/uf.controller.ts
+++ src/modules/uf/controller/uf.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UfService } from '../service/uf.service';
 import { UFNotFoundException } from '../domain/errors/UfNotFound.exception';
 import { AllExceptionsFilterDTO } from 'src/shared/domain/dtos/errors/AllException.filter.dto';
+import { FindUFsResponseDTO } from '../domain/requests/FindUfs.request.dto';
 
 @Controller('ufs')
 @ApiTags('UFs')
@@ -16,6 +17,11 @@ export class UfController {
         status: new UFNotFoundException().getStatus(),
         description: new UFNotFoundException().message,
         type: AllExceptionsFilterDTO
+    })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'UFs encontradas',
+        type: FindUFsResponseDTO
     })
     async findAll(): Promise<any> {
         return await this.ufService.findAll();
