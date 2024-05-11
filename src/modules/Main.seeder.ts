638f29d9e28c8892d2eac77c3c7548491034d2cc
Davi Batista <davideosmar11@gmail.com>
2024-05-10 22:08:00 -0300

    feat: added user seeder

--- src/modules/Main.seeder.ts
+++ src/modules/Main.seeder.ts
@@ -3,6 +3,7 @@ import { Seeder, SeederFactoryManager, runSeeder } from 'typeorm-extension';
 import UfSeeder from './uf/seeders/uf.seeder';
 import SkillSeeder from './skill/seeders/skill.seeder';
 import JobCategorySeeder from './job-category/seeders/job-category.seeder';
+import UserSeeder from './user/seeders/user-seeder';
 
 export class MainSeeder implements Seeder {
   async run(
@@ -12,5 +13,7 @@ export class MainSeeder implements Seeder {
     await runSeeder(dataSource, UfSeeder);
     //        await runSeeder(dataSource, SkillSeeder)
     await runSeeder(dataSource, JobCategorySeeder);
+
+    await runSeeder(dataSource, UserSeeder)
   }
 }
