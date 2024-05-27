e71169f8c3b5b0fd2b0d2fca27270f7e2d44d8fd
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-05-27 19:31:08 -0300

    test: created automated tests for ufs

--- src/modules/uf/service/uf.service.spec.ts
+++ src/modules/uf/service/uf.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UfService } from './uf.service';
+import { Uf } from '../entity/uf.entity';
+import { DatabaseModule } from '../../../database/database.module';
+import { UfModule } from '../uf.module';
+import UfSeeder from '../seeders/uf.seeder';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UfController } from '../controller/uf.controller';
 
 describe('UfService', () => {
   let service: UfService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        DatabaseModule,
+        UfModule,
+        UfSeeder,
+        Uf,
+        TypeOrmModule.forFeature([Uf]),
+      ],
       providers: [UfService],
+      controllers: [UfController],
     }).compile();
 
     service = module.get<UfService>(UfService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should bring all the UFs from the database', async () => {
+    const request = await service.findAll()
+
+    expect(request).toBeInstanceOf(Array)
+    
+    expect(typeof request[0].id_uf).toEqual("number")
+    expect(typeof request[0].name).toEqual("string")
+    expect(typeof request[0].acronym).toEqual("string")
   });
+
+  it('should contain 27 ufs in total if the database has been previously seeded', async () =>  {
+    const request = await service.findAll()
+
+    expect(request).toHaveLength(27)
+  })
 });
