f6b525dff2d0be611634ee5b87d75f9f4a1f2d2f
Davi Deosmar Batista Oliveira Miranda - 823212382 <823212382@ulife.com.br>
2024-05-27 20:38:13 -0300

    test: created automated tests for job category service

--- src/modules/job-category/service/job-category.service.spec.ts
+++ src/modules/job-category/service/job-category.service.spec.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobCategory } from '../entity/job-category.entity';
 import { Job } from '../../../modules/job/entity/job.entity';
 import { Skill } from '../../../modules/skill/entity/skill.entity';
+import { FindJobCategoryResponseDTO } from '../domain/requests/FindJobCategories.request.dto';
 
 describe('JobCategoryService', () => {
   let jobCategoryService: JobCategoryService;
@@ -28,8 +29,36 @@ describe('JobCategoryService', () => {
 
     expect(request).toBeInstanceOf(Array)
 
+    expect(request[0]).toHaveProperty("id_category")  
     expect(typeof request[0].id_category).toEqual("number")
+    expect(request[0]).toHaveProperty("name")  
     expect(typeof request[0].name).toEqual("string")
+    expect(request[0]).toHaveProperty("image_url")  
     expect(typeof request[0].image_url).toEqual("string")
   });
+
+  it('should contain 8 job categories in total if the database has been previously seeded', async () =>  {
+    const request = await jobCategoryService.findAll()
+
+    expect(request).toHaveLength(8)
+  })
+
+  it('should bring a single job category from the database', async ()  =>  {
+    const basalRequest = await jobCategoryService.findAll()
+
+    const request: FindJobCategoryResponseDTO = await jobCategoryService.findOne(basalRequest[0].id_category)
+
+    expect(request).toHaveProperty('id_category')
+    expect(typeof request.id_category).toEqual("number")
+    expect(request).toHaveProperty('name')
+    expect(typeof request.name).toEqual("string")
+    expect(request).toHaveProperty('image_url')
+    expect(typeof request.image_url).toEqual("string")
+  })
+
+  it('should throw an error if an unvalid number is passed on the individual job category finder', async ()  =>  {
+    expect(async () => {
+      await jobCategoryService.findOne(11111111111111111111111111999999999999)
+    }).rejects.toThrow(CategoryNotFoundException);
+  })
 });
