6fef045996a93a725c80c794af35a1d6372532e1
Davi Batista <davideosmar11@gmail.com>
2024-07-01 12:46:07 -0300

    fix: added address data on user profile service

--- src/modules/user/domain/requests/GetProfileData.request.dto.ts
+++ src/modules/user/domain/requests/GetProfileData.request.dto.ts
@@ -6,7 +6,18 @@ export const GetCandidateProfileDataResponseSchema = z.object({
     name: z.string().max(50).describe('Nome do usuário'),
     email: z.string().email().max(50).describe('E-mail do usuário'),
     role: z.string().min(7).max(9).describe('Papel do usuário'),
-    birth_date: z.string().length(10).describe('Data de nascimento do usuário')
+    birth_date: z.string().length(10).describe('Data de nascimento do usuário'),
+    address: z.object({
+        uf: z.object({
+            id: z.number().describe('ID da UF'),
+            name: z.string().max(50).describe('Nome da UF')
+        }),
+        city: z.string().max(50).describe('Cidade do endereço'),
+        cep: z.string().length(8).describe('CEP do endereço'),
+        street: z.string().max(50).describe('Rua do endereço'),
+        number: z.string().max(10).describe('Número do endereço'),
+        complement: z.string().max(50).nullable().describe('Complemento do endereço'),
+    }).describe('Endereço do usuário')
 })
 
 export class GetCandidateProfileDataResponseDTO extends createZodDto(
@@ -19,6 +30,17 @@ export const GetCompanyProfileDataResponseSchema = z.object({
     email: z.string().email().max(50).describe('E-mail do usuário'),
     role: z.string().min(7).max(9).describe('Papel do usuário'),
     cnpj: z.string().length(16).describe('CNPJ da empresa'),
+    address: z.object({
+        uf: z.object({
+            id: z.number().describe('ID da UF'),
+            name: z.string().max(50).describe('Nome da UF')
+        }),
+        city: z.string().max(50).describe('Cidade do endereço'),
+        cep: z.string().length(8).describe('CEP do endereço'),
+        street: z.string().max(50).describe('Rua do endereço'),
+        number: z.string().max(10).describe('Número do endereço'),
+        complement: z.string().max(50).nullable().describe('Complemento do endereço'),
+    }).describe('Endereço do usuário')
 })
 
 export class GetCompanyProfileDataResponseDTO extends createZodDto(
